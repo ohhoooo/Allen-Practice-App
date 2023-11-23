0832ebe25294727dd1c81f5e7309276febd701a0
KJH <gh7052@gmail.com>
2023-11-24 06:40:34 +0900

    스토리보드에서의 화면 이동 방법 - 다음 화면이 스토리보드로 되어 있을 때(간접 세그웨이)

--- NextVC/NextVC/ViewController.swift
+++ NextVC/NextVC/ViewController.swift
@@ -9,10 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     // 1) 코드로 화면 이동 (다음화면이 코드로 작성되어있을때만 가능한 방법)
@@ -33,19 +31,20 @@ class ViewController: UIViewController {
         self.present(secondVC, animated: true, completion: nil)
     }
     
-    
-    
     // 3) 스토리보드에서의 화면 이동(간접 세그웨이)
     @IBAction func storyboardWithSegueButtonTapped(_ sender: UIButton) {
-        
-
-        
-        
-        
-        
+        // 세그웨이를 실행한다는 의미의 메서드 호출
+        self.performSegue(withIdentifier: "toThirdVC", sender: self)
     }
     
-
-    
+    // 4) 스토리보드에서의 화면 이동(직접 세그웨이)
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //print(#function)
+        if segue.identifier == "toThirdVC" {
+            let thirdVC = segue.destination as! ThirdViewController
+            thirdVC.someString = "안녕하세요"
+            //thirdVC.mainLabel.text = "안녕하세요"    // 에러발생 (스토리보드 객체가 나중에 생김)
+        }
+    }
 }
 
