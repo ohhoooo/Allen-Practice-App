045a4451c8e8ea64239f0a0dcdebacc0d8cd022f
KJH <gh7052@gmail.com>
2023-11-26 11:44:25 +0900

    BMI 메소드 구현(BMI, 색, 결과 문자열), prepare 논리 구현

--- NextVC2/NextVC2/SecondViewController.swift
+++ NextVC2/NextVC2/SecondViewController.swift
@@ -13,17 +13,26 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var adviceLabel: UILabel!
     @IBOutlet weak var backButton: UIButton!
     
+    // 전화면에서 전달받은 데이터들
+    var bmiNumber: Double?
+    var adviceString: String?
+    var bmiColor: UIColor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 전화면에서 전달받은 데이터들을 통해 셋팅
+        bmiNumberLabel.text = "\(bmiNumber!)"
+        bmiNumberLabel.backgroundColor = bmiColor
+        adviceLabel.text = adviceString
+        
         configureUI()
     }
     
     func configureUI() {
         bmiNumberLabel.clipsToBounds = true
         bmiNumberLabel.layer.cornerRadius = 8
-        bmiNumberLabel.backgroundColor = .gray
-        
+//        bmiNumberLabel.backgroundColor = .gray
         
         backButton.clipsToBounds = true
         backButton.layer.cornerRadius = 5
--- NextVC2/NextVC2/ViewController.swift
+++ NextVC2/NextVC2/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
     
+    // BMI계산 결과값 보관위한 변수
+    var bmi: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,8 +34,14 @@ class ViewController: UIViewController {
         weightTextField.placeholder = "kg단위로 입력해주세요"
     }
 
+    // BMI계산하기 - 버튼 누르면(다음화면)
     @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        print(#function)
+        bmi = calculateBMI(height: heightTextField.text!, weight: weightTextField.text!)
         
+        
+        // 필요없음(버튼에서 직접 연결)
+        //performSegue(withIdentifier: "toSecondVC", sender: self)
     }
     
     // 조건에 따라 다음화면 이동할지/말지
@@ -55,10 +64,62 @@ class ViewController: UIViewController {
         if segue.identifier == "toSecondVC" {
             let secondVC = segue.destination as! SecondViewController
             secondVC.modalPresentationStyle = .fullScreen
+            
+            // 다음화면으로 데이터 전달
+            secondVC.bmiNumber = bmi
+            secondVC.bmiColor = getBackgroundColor()
+            secondVC.adviceString = getBMIAdviceString()
         }
         // 다음화면으로 가기전에 텍스트필드 비우기
         heightTextField.text = ""
         weightTextField.text = ""
+    }
+    
+    // BMI계산 메서드
+    func calculateBMI(height: String, weight: String) -> Double {
+        guard let h = Double(height), let w = Double(weight) else { return 0.0 }
+        var bmi = w / (h * h) * 10000
+        bmi = round(bmi * 10) / 10
+        print("BMI결과값: \(bmi)")
+        return bmi
+    }
+    
+    // 색깔 얻는 메서드
+    func getBackgroundColor() -> UIColor {
+        guard let bmi = bmi else { return UIColor.black }
+        switch bmi {
+        case ..<18.6:
+            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+        case 18.6..<23.0:
+            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+        case 23.0..<25.0:
+            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+        case 25.0..<30.0:
+            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+        case 30.0...:
+            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+        default:
+            return UIColor.black
+        }
+    }
+    
+    // 문자열 얻는 메서드
+    func getBMIAdviceString() -> String {
+        guard let bmi = bmi else { return "" }
+        switch bmi {
+        case ..<18.6:
+            return "저체중"
+        case 18.6..<23.0:
+            return "표준"
+        case 23.0..<25.0:
+            return "과체중"
+        case 25.0..<30.0:
+            return "중도비만"
+        case 30.0...:
+            return "고도비만"
+        default:
+            return ""
+        }
     }
 }
 
